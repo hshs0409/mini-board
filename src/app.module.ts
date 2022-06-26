dfbd910ef943d7f46e1e1983d6c6944efe4f8976
hshs0409 <0409hshs@gmail.com>
2022-06-26 20:53:33 +0900

    NestJS 기본 요소 필기

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BoardsModule } from './boards/boards.module';
 
+/*
+Nest JS 모듈이란?
+
+Module 데코레이터 주석이 달린 클래스. 
+Nest가 애플리케이션 구조를 구성하는데 사용하는 메타 데이터를 제공
+
+모듈은 밀접하게 관련된 기능 집합으로 구성 요소를 구성하는 효과적인 방법 (기능별로 분리)
+같은 기능에 해당하는 것들은 하나의 모듈 폴더 안에 넣어서 사용
+
+모듈은 기본적으로 싱글 톤이므로 여러 모듈간에 쉽게 공급자의 동일한 인스턴스 공유 가능
+
+데코레이터는 클래스에 함수 기능을 추가할 수 있다.
+클래스 위의 함수이고, 클래스를 위해 움직인다.
+ */
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BoardsModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// main
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
+/*
+EsLint => 특정한 규칙을 가지고 코드를 깔끔하게 짤 수 있도록 도와주는 라이브러리, 타입스크립트를 사용하는 가이드 라인 제시
+Prettier => 주로 코드 형식을 맞추는데 사용, 포맷터 역할
+nest-cli.json => nest 프로젝트 설정 json 파일
+tsconfig => 타입스크립트 컴파일 설정
+tsconfig.build => build할 때 필요한 설정 파일 exclude에서 제외할 파일 설정 가능
+*/
 bootstrap();
