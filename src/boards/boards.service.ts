9ac16939f26e07a03463a15ee9409bc83c8bd39c
hshs0409 <0409hshs@gmail.com>
2022-07-05 00:02:48 +0900

    Feat Add Exception in BoardService

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board } from './board.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -37,11 +37,17 @@ export class BoardsService {
   }
 
   getBoardById(id: string): Board {
-    return this.boards.find((board) => board.id === id);
+    const found = this.boards.find((board) => board.id === id);
+    if (!found) {
+      throw new NotFoundException(`해당하는 id : ${id}를 찾을 수 없습니다.`);
+    }
+    return found;
   }
 
   deleteBoard(id: string): void {
-    this.boards.filter((board) => board.id !== id);
+    const found = this.getBoardById(id);
+    this.boards = this.boards.filter((board) => board.id !== found.id);
+    return;
   }
 
   updateBoardStatus(updateBoardStatusDto: UpdateBoardStatusDto): Board {
